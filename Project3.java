cdb620df2ee82f0153d3175a25848bbd94828e2e
Mitch Merrell <themitchguy@gmail.com>
2025-08-21 20:45:20 -0700

    Project 3 done
    Project 3 completion

---
+++ Project3.java
@@ -0,0 +1,8 @@
+// project 3 outputs two differently precise values of pi
+
+public class Project3 {
+	public static void main(String args[]) {
+		System.out.println(4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+		System.out.println(4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13)));
+	}
+}
