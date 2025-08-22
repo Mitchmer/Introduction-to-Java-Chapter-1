45878eb7fdeaea3e4a90bc86796751022757ca36
Mitch Merrell <themitchguy@gmail.com>
2025-08-21 20:51:12 -0700

    Project 5 done
    Project 5 completion

---
+++ Project5.java
@@ -0,0 +1,7 @@
+// Project 5 outputs the result of an average acceleration calculation
+
+public class Project5 {
+	public static void main(String args[]) {
+		System.out.println((10.5-5.6)/0.5);
+	}
+}
